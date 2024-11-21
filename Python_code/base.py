efcf461ee68bbc6104b33f0f6d3042ad915d236a
svermeulen <vermeulen.stijn06@gmail.com>
2024-11-21 14:28:43 +0100

    added beginning Nov 21 journal, panel setup code

--- Python_code/base.py
+++ Python_code/base.py
@@ -5,6 +5,24 @@ import pandas
 import configparser
 
 
+def setup_panel():
+    # panel
+    panel.extension(design="material", sizing_mode="stretch_width")
+
+
+class Data:
+    # cache the data using panel:
+    @panel.cache
+    def __init__(self, config):
+        # read the data and set it in self directory's
+        self.all_frame = {
+            pandas.read_csv(config["FILE"])
+        }
+        self.roi_frame = {
+            pandas.read_csv(config["FILE"])
+        }
+
+
 def read_config(config_loc):
     config = configparser.ConfigParser()
     config.read(config_loc)
@@ -12,7 +30,9 @@ def read_config(config_loc):
 
 
 def main():
-    config = read_config('config.ini')
+    config = read_config("config.ini")
+    setup_panel()
+    Data(config)
 
 
 if __name__ == "__main__":
