b1f0c03a8f4010fae2a2bcdef550b066a8fa0457
svermeulen <vermeulen.stijn06@gmail.com>
2024-12-03 11:33:09 +0100

    added Journal to 3 Dec and combining of the dict to single data frame

--- Python_code/base.py
+++ Python_code/base.py
@@ -26,10 +26,11 @@ def panel_vis(data, chr_select, pos_slider_start, pos_slider_end):
 
 # No config is used at the moment
 
-# def read_config(config_loc):
-#     config = configparser.ConfigParser()
-#     config.read(config_loc)
-#     return config
+
+def read_config(config_loc):
+    config = configparser.ConfigParser()
+    config.read(config_loc)
+    return config
 
 
 def get_abs_paths(loc, ext):
@@ -46,6 +47,24 @@ def read_all_files(abs_files):
     return {file[-29:-20]: (pd.read_csv(file, sep="\t")) for file in abs_files}
 
 
+def combine_df_dict_to_df(df_dict):
+    # define empty dataframe to concat to
+    full_roi_data = pd.DataFrame(columns=["chr", "pos", "gene", "barcode"])
+
+    for barcode in df_dict:
+
+        # create a copy to manipulate
+        barcodex = df_dict[barcode].copy()
+
+        # create a new column to get the patient barcode in te combined dataframe
+        barcodex.insert(len(barcodex.columns), "barcode", barcode)
+
+        # concat the data on top of the full_roi_data dataframe
+        full_roi_data = pd.concat([full_roi_data, barcodex], join="inner", ignore_index=True, sort=False)
+
+    return full_roi_data
+
+
 class Data:
     # cache the data using panel:
     @pn.cache
@@ -53,15 +72,18 @@ class Data:
         # read the data and set it in self directory's
         # all_paths = get_abs_paths(path, "ALL.csv")
         roi_paths = get_abs_paths(path, "ROIs.csv")
-        # self.all_dict = read_all_files(all_paths)
-        self.roi_dict = read_roi_files(roi_paths)
+        # all_dict = read_all_files(all_paths)
+        roi_dict = read_roi_files(roi_paths)
+        # self.all_data = combine_df_dict_to_df(all_dict)
+        self.roi_data = combine_df_dict_to_df(roi_dict)
 
 
 def main():
     # config = read_config("config.ini")
     info = Data("Data/analysis")
-    [chr_s, pos_ss, pos_se] = setup_panel(info)
-    panel_vis(info, chr_s, pos_ss, pos_se)
+    # [chr_s, pos_ss, pos_se] = setup_panel(info)
+    # panel_vis(info, chr_s, pos_ss, pos_se)
+    print(info.roi_data)
 
 
 if __name__ == "__main__":
