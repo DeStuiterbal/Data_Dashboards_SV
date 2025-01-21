68342dd87b708a6bbe1632256c9256454d80f474
svermeulen <vermeulen.stijn06@gmail.com>
2025-01-21 09:01:52 +0100

    added density plot changed ranges on chromosome to unchangeble for the time being

--- Python_code/viewing.py
+++ Python_code/viewing.py
@@ -1,28 +1,28 @@
+import matplotlib
 import matplotlib.pyplot as plt
 import panel as pn
 from panel.viewable import Viewer, Viewable
 import param
 import pandas as pd
+import seaborn as sns
 
 
 class View(Viewer):
     pn.extension(design="material", sizing_mode="stretch_width")
+    matplotlib.use("agg")
 
     data = param.DataFrame()
     genes = param.DataFrame()
 
-    gene_input = param.String(default="")
+    gene_input = param.String(default="", label="Input gene name")
 
     # set default to empty string because values are determined by other input values
-    gene_name = param.ObjectSelector(objects=[None], allow_None=True)
+    gene_name = param.ObjectSelector(objects=[None], allow_None=True, label="Select gene")
 
     # define input for all the chromosomes
     chromosome = param.ListSelector(objects=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13",
-                                             "14", "15", "16", "17", "18", "19", "20", "21", "22", "X", "Y"])
-
-    # define range on chromosome
-    chrom_range_down = pn.widgets.IntInput(value=0, start=0, name="start chromosome region")
-    chrom_range_up = pn.widgets.IntInput(value=0, start=0, name="end chromosome region")
+                                             "14", "15", "16", "17", "18", "19", "20", "21", "22", "X", "Y"],
+                                    label="Selected chromosome(s)")
 
     def __init__(self, **params):
         super().__init__(**params)
@@ -31,49 +31,41 @@ class View(Viewer):
 
         self.gene_selectables = list(set(self.genes["Symbol"]))
 
-        # set chrom bounds
-        chrom_bound_up = max(self.data["end"] + 5)
-        self.chrom_range_down.end = chrom_bound_up
-        self.chrom_range_up.end = chrom_bound_up
+        # chrom range is to give the ranges for each chromosome
+        self.chrom_range = {}
 
-        # self.all_pipeline = self.data.idf[
-        #     (self.data['start'] >= self.chrom_range_down.value) &
-        #     (self.data['end'] <= self.chrom_range_up.value) &
-        #     (self.data['chr'].isin(["chr" + chrom for chrom in self.chromosome]))
-        # ]
-
-    @param.depends("gene_name", "genes", "chromosome", "chrom_range_down", "chrom_range_up")
+    @param.depends("gene_name", "genes")
     def get_pro_region(self):
         """
 
         :return: promoter and chromosome location of the given gene
         """
+        self.chrom_range = {}
         input_gene_info = self.genes[self.genes["Symbol"] == self.gene_name]
-
-        # get the beginning and end location of the gene
-        promoter = [input_gene_info["Begin"].tolist()[0], input_gene_info["End"].tolist()[0]]
-
-        # add or subtract from the promoter depending on the orientation of the gene
-        if input_gene_info["Orientation"].tolist()[0] == "plus":
-            promoter[0] -= 1000
-        else:
-            promoter[1] += 1000
-
-        # get the chromosome of the gene
+        # get the chromosomes of the genes
         self.chromosome = input_gene_info['Chromosome'].tolist()
 
-        self.chrom_range_down.value = promoter[0]
-        self.chrom_range_up.value = promoter[1]
+        for i, chrom in enumerate(self.chromosome):
+            # get the beginning and end location of the gene
+            promoter = [input_gene_info["Begin"].tolist()[i], input_gene_info["End"].tolist()[i]]
 
-    @param.depends("data", "chromosome", "chrom_range_down", "chrom_range_up")
+            # add or subtract from the promoter depending on the orientation of the gene
+            if input_gene_info["Orientation"].tolist()[i] == "plus":
+                promoter[0] -= 1000
+            else:
+                promoter[1] += 1000
+
+            self.chrom_range.update({chrom: promoter})
+
+    @param.depends("data", "chromosome")
     def filter_on_pro(self):
         """
 
         :return:
         """
-        return self.data[(self.data['start'] >= self.chrom_range_down.value) &
-                         (self.data['end'] <= self.chrom_range_up.value) &
-                         (self.data['chr'].isin(["chr" + chrom for chrom in self.chromosome]))]
+        return [self.data[(self.data['start'] >= self.chrom_range[chrom][0]) &
+                          (self.data['end'] <= self.chrom_range[chrom][1]) &
+                          (self.data['chr'] == ("chr" + chrom))] for chrom in self.chromosome]
 
     @param.depends("genes", "gene_input")
     def filter_by_gene_input(self):
@@ -92,62 +84,96 @@ class View(Viewer):
         if self.chromosome is None or self.chromosome == []:
             self.gene_selectables = list(set(self.genes["Symbol"]))
         else:
-            self.gene_selectables = list(set(self.genes[self.genes["Chromosome"].isin(self.chromosome)]["Symbol"]))
+            selectable = []
+            for chromosome in self.chromosome:
+                if selectable:
+                    temp = list(set(self.genes[self.genes["Chromosome"] == chromosome]["Symbol"]))
+                    selectable = [gene for gene in selectable if gene in temp]
+                else:
+                    selectable = list(set(self.genes[self.genes["Chromosome"] == chromosome]["Symbol"]))
+            self.gene_selectables = selectable
 
     @param.depends("gene_name", "genes", "chromosome", "gene_input")
     def set_options(self):
         self.filter_by_chrom()
         self.filter_by_gene_input()
-        if len(self.gene_selectables) > 1000:
+        if len(self.gene_selectables) > 200:
             self.param.gene_name.objects = [None]
         else:
             self.gene_selectables.sort()
             self.param.gene_name.objects = [None] + self.gene_selectables
 
-    @param.depends("data", "gene_name")
+    @param.depends("data", "gene_name", "chromosome")
     def update_data(self):
         if self.gene_name is not None:
             self.get_pro_region()
-            return self.filter_on_pro()
-        elif self.chromosome & (self.chrom_range_down.value - self.chrom_range_up.value) > 1000:
             return self.filter_on_pro()
         return None
 
     @staticmethod
     def create_barcode_counts(df, all_barcode):
+        """
+
+        :param df:
+        :param all_barcode:
+        :return:
+        """
         counts = []
         for barcode in all_barcode:
             counts.append(df["barcode"].to_list().count(barcode))
 
-        fig, ax = plt.subplots(figsize=(4, 3))
+        fig, ax = plt.subplots(figsize=(3, 3))
         ax.bar(all_barcode, counts)
         ax.set(title="Barcode Counts")
 
         plt.setp(ax.get_xticklabels(), rotation=15, horizontalalignment='center', fontsize='x-small')
-
+        plt.close(fig)
         return fig
 
-    @param.depends("data", "chrom_range_down", "chrom_range_up")
+    @param.depends("data", "chromosome")
     def show_coords(self):
-        df = self.update_data()
-        if df is not None:
-            x_range = range(self.chrom_range_down.value, self.chrom_range_up.value)
-            all_barcode = list(set(df["barcode"]))
-            all_barcode.sort()
+        """
 
-            barcode_bar = self.create_barcode_counts(df, all_barcode)
+        :return:
+        """
+        dfs = self.update_data()
+        barcode_bar = []
+        density_fig = []
+        if dfs is not None:
+            for i, chromosome in enumerate(self.chromosome):
+                print(chromosome)
+                df = dfs[i]
+                all_barcode = list(set(df["barcode"]))
+                all_barcode.sort()
 
-            return barcode_bar
+                # density plot
+                fig, density_ax = plt.subplots(figsize=(10, 4))
+                sns.kdeplot(data=df, x="start", hue="barcode")
+                density_ax.set(title="Methylation density", xlabel="Position (bp)")
+                plt.close(fig)
+                density_fig.append(fig)
 
-    @param.depends("gene_input", "chromosome", "gene_name", "chrom_range_down", "chrom_range_up")
+                barcode_bar.append(self.create_barcode_counts(df, all_barcode))
+
+        figures = [barcode_bar, density_fig]
+
+        return figures
+
+    @param.depends("gene_input", "chromosome", "gene_name")
     def __panel__(self) -> Viewable:
         self.set_options()
-        bar = self.show_coords()
-        return pn.Column(pn.Row((pn.widgets.MultiChoice.from_param(self.param.chromosome))),
-                         pn.Row(self.chrom_range_down, self.chrom_range_up),
-                         pn.Row(pn.widgets.TextInput.from_param(self.param.gene_input),
-                                pn.widgets.Select.from_param(self.param.gene_name)),
-                         pn.Row(pn.pane.Matplotlib(bar)))
+        figs = self.show_coords()
+        visualise = pn.Column(pn.Row((pn.widgets.MultiChoice.from_param(self.param.chromosome))),
+                              pn.Row(pn.widgets.TextInput.from_param(self.param.gene_input),
+                                     pn.widgets.Select.from_param(self.param.gene_name)))
+        if self.chromosome is not None:
+            for i, pic in enumerate(figs[1]):
+                print(i)
+                visualise.append(pn.Row(pn.pane.Str("Chromosome " + self.chromosome[i], styles={"font-size": "20pt"},
+                                                    align="center")))
+                visualise.append(pn.Row(pn.pane.Matplotlib(figs[0][i])))
+                visualise.append(pn.Row(pn.pane.Matplotlib(figs[1][i])))
+        return visualise
 
     def __str__(self):
         return """
